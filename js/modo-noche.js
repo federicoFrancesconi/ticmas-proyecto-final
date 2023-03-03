770be11f8b49f72988c18e64dcb8bbc6c324bedb
Federico Francesconi <108153799+federicoFrancesconi@users.noreply.github.com>
2023-03-02 22:48:31 -0300

    Reformulacion del modo oscuro

---
+++ js/modo-noche.js
@@ -0,0 +1,30 @@
+const body = document.querySelector('body');
+const boton = document.querySelector('#boton_cambio_modo');
+
+const textoBotonClaro = '<i id="sol" class="fa-sharp fa-solid fa-sun"></i> Modo Claro';
+const textoBotonOscuro = '<i id="luna" class="fa-solid fa-moon"></i> Modo Oscuro';
+
+let estado;
+
+boton.addEventListener('click', x => {
+    //Si la clase estaba agregada la quito, y si no lo estaba la agrego
+    body.classList.toggle('darkmode');
+
+    //Guardo el estado actual en una constante
+    const estado = body.classList.contains('darkmode');
+
+    //Tambien lo guardo en localstorage
+    localStorage.setItem('modo', estado);
+
+    //Hago el cambio del texto del boton que no me permite CSS
+    boton.innerHTML = estado ? textoBotonClaro : textoBotonOscuro;
+});
+
+window.onload = () => {
+    let modoActual = localStorage.getItem('modo') || 0;
+    console.log(modoActual);
+    if (modoActual == 'true') {
+        body.classList.add('darkmode');
+        boton.innerHTML = textoBotonClaro;
+    }
+}
