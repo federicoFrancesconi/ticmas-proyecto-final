a5d4f57e1fabcc1134d4cfd0b86ce502b293dfb0
Federico Francesconi <108153799+federicoFrancesconi@users.noreply.github.com>
2023-03-04 22:43:13 -0300

    avance media queries

--- js/menu.js
+++ js/menu.js
@@ -1,4 +1,6 @@
 const nav = document.querySelector('nav');
+const iconoMenu = document.querySelector('#icono_menu');
+const iconoCruz = document.querySelector('#icono_cruz');
 
 document.getElementById('menu').onclick = () => {
     desplegarMenu();
@@ -9,15 +11,23 @@ document.getElementById('cruz').onclick = () => {
 }
 
 nav.addEventListener('click', function() {
-    if (window.innerWidth <= 600) {
+    if (window.innerWidth <= 992) {
         ocultarMenu();
     }
 });
 
 window.onresize = () => {
-    if (window.innerWidth > 600) {
+    if (window.innerWidth > 992) {
         desplegarMenu();
     } else {
+        //Agrando el icono del menu entre 992 y 768
+        if (window.innerWidth > 768) {
+            iconoMenu.classList.add('fa-2x');
+            iconoCruz.classList.add('fa-2x');
+        } else {
+            iconoMenu.classList.remove('fa-2x');
+            iconoCruz.classList.remove('fa-2x');
+        }
         ocultarMenu();
     }
 }
